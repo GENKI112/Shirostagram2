48106f7ca5541abe620e1abac0feebcb9963f2b6
genki112 <67961141+GENKI112@users.noreply.github.com>
2021-02-25 22:25:55 +0900

    input_hashの削除、{}の中の両端にスペースを空けました

--- db/migrate/20210211122827_create_posts.rb
+++ db/migrate/20210211122827_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
 
       t.string :caption
-      t.text :input_hash
       t.references :user, foreign_key: true, null: false
 
       t.timestamps
--- db/migrate/20210224152233_create_post_taggings.rb
+++ db/migrate/20210224152233_create_post_taggings.rb
@@ -1,7 +1,7 @@
 class CreatePostTaggings < ActiveRecord::Migration[6.1]
   def change
     create_table :post_taggings do |t|
-      t.references :post, foreign_key: {on_delete: :cascade}, comment: "Postテーブルとの関連付け"
+      t.references :post, foreign_key: { on_delete: :cascade }, comment: "Postテーブルとの関連付け"
       t.references :hashtag, foreign_key: true, comment: "Hashtagテーブルとの関連付け"
 
       t.timestamps

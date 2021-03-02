ae3b699dddec4ae6efbb2be80e999a1941dbfecf
genki112 <67961141+GENKI112@users.noreply.github.com>
2021-03-02 21:03:51 +0900

    コールバックの修正と、正規表現のメソッド定義

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,5 @@
 module PostsHelper
-
   def render_with_hashtags(caption)
     caption.gsub(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/){|word| link_to word, "/post/hashtag/#{word.delete("#")}"}.html_safe
   end
-  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,30 +8,21 @@ class Post < ApplicationRecord
   has_many :hashtags, through: :post_taggings
 
   accepts_nested_attributes_for :photos
+  after_create :generate_hashtag
 
   def liked_by(user)
     Like.find_by(user_id: user.id, post_id: id)
   end
 
-  after_create do
-    # 作成した投稿を探す
-    post = Post.find_by(id: self.id)
-    hashtags = post.caption.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-    hashtags.uniq.map do |hashtag|
-      # ハッシュタグは先頭の'#'を外した上で保存するようにする
-      tag = Hashtag.find_or_create_by(label: hashtag.downcase.delete('#'))
-      post.hashtags << tag
-    end
+  def hashtag_scan
+    caption.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
   end
 
-  before_update do 
-    post = Post.find_by(id: self.id)
-    post.hashtags.clear
-    hashtags = post.caption.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-    hashtags.uniq.map do |hashtag|
-      tag = Hashtag.find_or_create_by(label: hashtag.downcase.delete('#'))
-      post.hashtags << tag
+  private
+    def generate_hashtag
+      post_labels = self.hashtag_scan
+      post_labels.uniq.map do |hashtag|
+      self.hashtags <<  Hashtag.find_or_create_by(label: hashtag.delete('#'))
     end
   end
-  
 end

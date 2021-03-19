75e23243baaec3beac82be4483d75bc3ef58fbec
genki112 <67961141+GENKI112@users.noreply.github.com>
2021-03-20 08:19:03 +0900

    hashtagごとのルートを設定し、対応するビューを作成

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,4 +1,7 @@
 class HashtagsController < ApplicationController
   def index
+    @label = params[:label_name]
+    post_in_label = Hashtag.find_by(label: params[:label_name])
+    @posts = post_in_label.posts.order('created_at DESC')
   end
 end

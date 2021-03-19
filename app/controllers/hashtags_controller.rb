1865c03efb445e74a43b151ce4ffe83bf931e5ab
genki112 <67961141+GENKI112@users.noreply.github.com>
2021-03-20 08:30:41 +0900

    posts/hashtag/:label_nameのページはアカウント登録しないと見れないように設定

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,4 +1,5 @@
 class HashtagsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @label = params[:label_name]
     post_in_label = Hashtag.find_by(label: params[:label_name])
